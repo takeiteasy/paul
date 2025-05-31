a64b12e04fd62ff11d3969ec1cda70ee5aebb501
George Watson <gigolo@hotmail.co.uk>
2025-05-31 12:21:36 +0100

    delete cimgui

--- deps/cimgui/backend_test/example_glfw_dx11/README.md
+++
@@ -1,6 +0,0 @@
-
-This example needs dx11 in generation before compile. (add dx11 to generator.bat(sh) and generate)
-
-`STATIC_BUILD` is the cmake variable to do static linking
-
-Only tested with VC nmake.
--- src/jeff.h
+++ src/jeff.h
@@ -15,116 +15,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
-#ifndef JEFF_HEAD // 👺
-#define JEFF_HEAD // 👺
+#ifndef JEFF_HEAD
+#define JEFF_HEAD
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifdef _MSC_VER
-#define COMPILER_CL
-
-#ifdef _WIN32
-#define PLATFORM_WINDOWS
-#else
-#error compiler/platform combo is not supported yet
-#endif
-
-#if defined(_M_AMD64)
-#define ARCH_X64
-#elif defined(_M_IX86)
-#define ARCH_X86
-#elif defined(_M_ARM64)
-#define ARCH_ARM64
-#elif defined(_M_ARM)
-#define ARCH_ARM32
-#else
-#error architecture not supported yet
-#endif
-
-#if _MSC_VER >= 1920
-#define COMPILER_CL_YEAR 2019
-#elif _MSC_VER >= 1910
-#define COMPILER_CL_YEAR 2017
-#elif _MSC_VER >= 1900
-#define COMPILER_CL_YEAR 2015
-#elif _MSC_VER >= 1800
-#define COMPILER_CL_YEAR 2013
-#elif _MSC_VER >= 1700
-#define COMPILER_CL_YEAR 2012
-#elif _MSC_VER >= 1600
-#define COMPILER_CL_YEAR 2010
-#elif _MSC_VER >= 1500
-#define COMPILER_CL_YEAR 2008
-#elif _MSC_VER >= 1400
-#define COMPILER_CL_YEAR 2005
-#else
-#define COMPILER_CL_YEAR 0
-#endif
-// defined(_MSC_VER)
-#elif defined(__clang__)
-#define COMPILER_CLANG
-
-#if defined(__APPLE__) && defined(__MACH__)
-#define PLATFORM_MAC
-#elif defined(__gnu_linux__)
-#define PLATFORM_LINUX
-#else
-#error compiler/platform combo is not supported yet
-#endif
-
-#if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64)
-#define ARCH_X64
-#elif defined(i386) || defined(__i386) || defined(__i386__)
-#define ARCH_X86
-#elif defined(__aarch64__)
-#define ARCH_ARM64
-#elif defined(__arm__)
-#define ARCH_ARM32
-#else
-#error architecture not supported yet
-#endif
-// defined(__clang__)
-#elif defined(__GNUC__) || defined(__GNUG__)
-#define COMPILER_GCC
-
-#ifdef __gnu_linux__
-#define PLATFORM_LINUX
-#else
-#error compiler/platform combo is not supported yet
-#endif
-
-#if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64)
-#define ARCH_X64
-#elif defined(i386) || defined(__i386) || defined(__i386__)
-#define ARCH_X86
-#elif defined(__aarch64__)
-#define ARCH_ARM64
-#elif defined(__arm__)
-#define ARCH_ARM32
-#else
-#error architecture not supported yet
-#endif
-#else
-#error compiler is not supported yet
-#endif
-
-#if defined(ARCH_X64)
-#define ARCH_64BIT
-#elif defined(ARCH_X86)
-#define ARCH_32BIT
-// defined(__GNUC__) || defined(__GNUP__)
-#endif
-
-#ifdef __cplusplus
-#define LANG_CPP
-#else
-#define LANG_C
-#endif
-
-#ifndef PLATFORM_WINDOWS
-#define PLATFORM_POSIX
-#endif
+#include "paul/paul.h"
 
 #ifndef __has_include
 #define __has_include(x) 1
@@ -154,7 +51,7 @@ extern "C" {
 #ifdef JEFF_NAME
 #define DEFAULT_WINDOW_TITLE JEFF_NAME
 #else
-#define DEFAULT_WINDOW_TITLE "👺"
+#define DEFAULT_WINDOW_TITLE "jeff"
 #endif
 #endif
 
@@ -692,4 +589,4 @@ void thread_queue_destroy(thread_queue_t *queue);
 #ifdef __cplusplus
 }
 #endif
-#endif // JEFF_HEAD 👺
+#endif // JEFF_HEAD
